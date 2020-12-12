142ccd175f183cde8edf41f6b1ff4a0f4ba447cf
efe <adambldwn110@gmail.com>
2020-12-12 18:30:36 +0300

    auth email

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {
   SafeAreaView,
-  Dimensions,
   View,
+  Alert,
   Text,
   Image,
   ScrollView,
@@ -12,12 +12,29 @@ import {
 import {authStyle} from './styles';
 import {Input, Button} from '../components';
 import auth from '@react-native-firebase/auth';
+import { resolveAuthError } from '../functions';
 
 const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function login(){
+    // auth()
+    // .signInWithEmailAndPassword(email,password)
+    // .then(()=> alert("OK"))
+    // .catch((err)=> Alert.alert("Chat", resolveAuthError(err.code)))
+    try {
+      await auth().signInWithEmailAndPassword(email,password)
+    } catch (error) {
+      Alert.alert("Chat", resolveAuthError(error.code));
+    }
+  }
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <KeyboardAvoidingView style={{flex: 1, backgroundColor: '#cfd8dc'}}>
         <ScrollView contentContainerStyle={{flex: 1}}>
+
           <View style={authStyle.container}>
             <Image
               style={authStyle.logo}
@@ -25,24 +42,30 @@ const Login = (props) => {
             />
             <Text style={authStyle.logoText}>CHAT</Text>
           </View>
+
           <View style={{ flex:1 }}>
+
             <Input
               inputProps={{
                 placeholder:"Type your email adress...",
                 keyboardType: "email-address"
               }}
+              onType={value => setEmail(value)}
             />
+
             <Input
               inputProps={{
                 placeholder:"Type your password..",
                 secureTextEntry: true
               }}
+              onType={value => setPassword(value)}
             />
 
-            <Button title="Sign In"/>
+            <Button title="Sign In" onPress={()=> login()} />
             <Button title="Sign Up" noBorder onPress={()=> props.navigation.navigate("Sign")}/>
             
           </View>
+
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
